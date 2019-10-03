808152810f397a78f78f45c011e8f3197cb0f8c1
JoseEiso <jose.benitez@eiso.com.co>
2019-10-03 17:42:56 -0500

    bootstrap agregado

--- yii_f1.1/votacionesApp/protected/views/layouts/main.php
+++ yii_f1.1/votacionesApp/protected/views/layouts/main.php
@@ -12,43 +12,41 @@
 
 	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
 </head>
-
 <body>
-
-<div class="container" id="page">
-
-	<div id="header">
-		<div id="logo"><?php echo CHtml::encode(Yii::app()->name); ?></div>
-	</div><!-- header -->
-
-	<div id="mainmenu">
-		<?php $this->widget('zii.widgets.CMenu',array(
-			'items'=>array(
-				array('label'=>'Home', 'url'=>array('/site/index')),
-				array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
-				array('label'=>'Contact', 'url'=>array('/site/contact')),
-				array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
-				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
-			),
-		)); ?>
+	<div class="navbar navbar-expand-lg navbar-light bg-light">
+		<div class="navbar-inner">
+			<div class="container">
+			<button type="button" class="btn btn-navbar" data-toggle="collapse" data-target= ".nav-collapse">
+				<span class="icon bar"></span>
+				<span class="icon bar"></span>
+				<span class="icon bar"></span>
+			</button>
+			<a class="brand" href="<?php echo yii::app()->homeUrl;?>">
+				<?php echo yii::app()->name;?>
+			</a>
+				<div class="nav-collapse collapse">
+				<?php $this->widget('zii.widgets.CMenu',array(
+					'items'=>array(
+						array('label'=>'Login', 'url'=>array('/usuario/login'), ),
+						array('label'=>'Gestion de empleados', 'url'=>array('/usuario/manager'), 'visible'=> (Yii::app()->session['login']['perfil'] === '1' ? true : false)),
+						array('label'=>'Gestión de areas', 'url'=>array('/area/index') , 'visible'=> (Yii::app()->session['login']['perfil'] === '1' ? true : false)),
+						array('label'=>'Gestión de empresas', 'url'=>array('/empresa/index'), 'visible'=> (Yii::app()->session['login']['perfil'] === '1' ? true : false)),
+						array('label'=>'Gestión de actividades', 'url'=>array('/actividad/create'), 'visible'=> (Yii::app()->session['login']['perfil'] === '1' ? true : false)),
+						array('label'=>'Logout ID:('.Yii::app()->session['login']['usuarioId'].')', 'url'=>array('/site/logout'), 'visible'=> (Yii::app()->session['login'] !== null ? true : false))
+					),
+					'htmlOptions' => array('class' => 'nav navbar-nav')
+				)); ?>
+				</div>
+			</div>
+		</div>
 	</div><!-- mainmenu -->
-	<?php if(isset($this->breadcrumbs)):?>
-		<?php $this->widget('zii.widgets.CBreadcrumbs', array(
-			'links'=>$this->breadcrumbs,
-		)); ?><!-- breadcrumbs -->
-	<?php endif?>
-
-	<?php echo $content; ?>
-
-	<div class="clear"></div>
-
-	<div id="footer">
-		Copyright &copy; <?php echo date('Y'); ?> by My Company.<br/>
-		All Rights Reserved.<br/>
-		<?php echo Yii::powered(); ?>
-	</div><!-- footer -->
-
-</div><!-- page -->
-
+	<div class="container">
+		<?php if(isset($this->breadcrumbs)):?>
+			<?php $this->widget('zii.widgets.CBreadcrumbs', array(
+				'links'=>$this->breadcrumbs,
+			)); ?><!-- breadcrumbs -->
+		<?php endif?>
+		<?php echo $content; ?>
+	</div>
 </body>
 </html>
